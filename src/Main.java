3f9a16f82a0c4eb34cbceb5ef131c740e334963f
yuusuke <yuusukeogawara@gmail.com>
2019-01-25 23:37:31 +0900

    カエルの大合唱を実装

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,123 @@
-import util.SpicaWrapper;
+import util.UdpSend;
 
 public class Main {
 
+
     public static void main(String[] args) {
-        SpicaWrapper spica = new SpicaWrapper("192.168.10.101",10000);
+
+        UdpSend udpSend1 = new UdpSend("192.168.1.101", 10000);
+        UdpSend udpSend2 = new UdpSend("192.168.1.102", 10000);
+        UdpSend udpSend3 = new UdpSend("192.168.1.103", 10000);
+        UdpSend udpSend4 = new UdpSend("192.168.1.104", 10000);
+        UdpSend udpSend5 = new UdpSend("192.168.1.105", 10000);
+        UdpSend udpSend6 = new UdpSend("192.168.1.106", 10000);
+        UdpSend udpSend7 = new UdpSend("192.168.1.107", 10000);
+        UdpSend udpSend8 = new UdpSend("192.168.1.108", 10000);
+        UdpSend udpSend9 = new UdpSend("192.168.1.109", 10000);
+        UdpSend udpSend10 = new UdpSend("192.168.1.110", 10000);
+        UdpSend udpSend11 = new UdpSend("192.168.1.111", 10000);
+        UdpSend udpSend12 = new UdpSend("192.168.1.112", 10000);
+        UdpSend udpSend13 = new UdpSend("192.168.1.113", 10000);
+        UdpSend udpSend14 = new UdpSend("192.168.1.114", 10000);
+        UdpSend udpSend15 = new UdpSend("192.168.1.115", 10000);
+        UdpSend udpSend16 = new UdpSend("192.168.1.116", 10000);
+        UdpSend udpSend17 = new UdpSend("192.168.1.117", 10000);
+        UdpSend udpSend18 = new UdpSend("192.168.1.118", 10000);
+        UdpSend udpSend19 = new UdpSend("192.168.1.119", 10000);
+        UdpSend udpSend20 = new UdpSend("192.168.1.120", 10000);
+        UdpSend udpSend21 = new UdpSend("192.168.1.121", 10000);
+        UdpSend udpSend22 = new UdpSend("192.168.1.122", 10000);
+        UdpSend udpSend23 = new UdpSend("192.168.1.123", 10000);
+        UdpSend udpSend24 = new UdpSend("192.168.1.124", 10000);
+        UdpSend udpSend25 = new UdpSend("192.168.1.125", 10000);
+        UdpSend udpSend26 = new UdpSend("192.168.1.126", 10000);
+        UdpSend udpSend27 = new UdpSend("192.168.1.127", 10000);
+        UdpSend udpSend28 = new UdpSend("192.168.1.128", 10000);
+        UdpSend udpSend29 = new UdpSend("192.168.1.129", 10000);
+        UdpSend udpSend30 = new UdpSend("192.168.1.130", 10000);
+        UdpSend udpSend31 = new UdpSend("192.168.1.131", 10000);
+        UdpSend udpSend32 = new UdpSend("192.168.1.132", 10000);
+        UdpSend udpSend33 = new UdpSend("192.168.1.133", 10000);
+        UdpSend udpSend34 = new UdpSend("192.168.1.134", 10000);
+        UdpSend udpSend35 = new UdpSend("192.168.1.135", 10000);
+        UdpSend udpSend36 = new UdpSend("192.168.1.136", 10000);
+        UdpSend udpSend37 = new UdpSend("192.168.1.137", 10000);
+        UdpSend udpSend38 = new UdpSend("192.168.1.138", 10000);
+        UdpSend udpSend39 = new UdpSend("192.168.1.139", 10000);
+        UdpSend udpSend40 = new UdpSend("192.168.1.140", 10000);
+        UdpSend udpSend50 = new UdpSend("192.168.1.150", 10000);
+
+
+
+        for (int i = 0; i<10000;i++) {
+            //udpSend1.UdpSendText("0001001001010");
+            udpSend2.UdpSendText("0001001001010");
+            udpSend3.UdpSendText("0001001001010");
+            //udpSend4.UdpSendText("0001001001010");
+            udpSend5.UdpSendText("0001001001010");
+            udpSend6.UdpSendText("0001001001010");
+            udpSend7.UdpSendText("0001001001010");
+            udpSend8.UdpSendText("0001001001010");
+            udpSend9.UdpSendText("0001001001010");
+            udpSend10.UdpSendText("0001001001010");
+            udpSend11.UdpSendText("0001001001010");
+            udpSend12.UdpSendText("0001001001010");
+            udpSend13.UdpSendText("0001001001010");
+            udpSend14.UdpSendText("0001001001010");
+            udpSend15.UdpSendText("0001001001010");
+            udpSend16.UdpSendText("0001001001010");
+            udpSend17.UdpSendText("0001001001010");
+            udpSend18.UdpSendText("0001001001010");
+            udpSend19.UdpSendText("0001001001010");
+            udpSend20.UdpSendText("0001001001010");
+            udpSend21.UdpSendText("0001001001010");
+            udpSend22.UdpSendText("0001001001010");
+            udpSend23.UdpSendText("0001001001010");
+            udpSend24.UdpSendText("0001001001010");
+            udpSend25.UdpSendText("0001001001010");
+            udpSend26.UdpSendText("0001001001010");
+            udpSend27.UdpSendText("0001001001010");
+            udpSend28.UdpSendText("0001001001010");
+            udpSend29.UdpSendText("0001001001010");
+            udpSend30.UdpSendText("0001001001010");
+            udpSend31.UdpSendText("0001001001010");
+            udpSend32.UdpSendText("0001001001010");
+            udpSend33.UdpSendText("0001001001010");
+            udpSend34.UdpSendText("0001001001010");
+            udpSend35.UdpSendText("0001001001010");
+            udpSend36.UdpSendText("0001001001010");
+            udpSend37.UdpSendText("0001001001010");
+            udpSend38.UdpSendText("0001001001010");
+            udpSend39.UdpSendText("0001001001010");
+            udpSend40.UdpSendText("0001001001010");
+            udpSend50.UdpSendText("0001001001010");
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+        //SpicaWrapper spica = new SpicaWrapper("192.168.10.101",10000);
 
 
         //////////////////この下にプログラムを書こう！/////////////////////
-        spica.front(1000,100);
-        spica.back(1000,80);
-
-        // しきい値 int型, 以上以下 String型
-        //spica.ifStartBlock(20,"up");
-        spica.left(1000,100);
-        //spica.elseBlock();
-        spica.right(1000,100);
-        //spica.ifEndBlock();
-        //spica.back(1500,100);
-
-
-        ///////////////////////////ここまで！///////////////////////////
-
-        spica.run();
+//        spica.front(1000,100);
+//        spica.back(1000,80);
+//
+//        // しきい値 int型, 以上以下 String型
+//        //spica.ifStartBlock(20,"up");
+//        spica.left(1000,100);
+//        //spica.elseBlock();
+//        spica.right(1000,100);
+//        //spica.ifEndBlock();
+//        //spica.back(1500,100);
+//
+//
+//        ///////////////////////////ここまで！///////////////////////////
+//
+//        spica.run();
     }
 }
